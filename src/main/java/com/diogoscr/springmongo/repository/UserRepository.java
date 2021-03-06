a291a1f78719d1c48c0df937494fc1bf5ab46f91
diogoscr <diogo.arauj@gmail.com>
2021-03-06 11:00:09 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/diogoscr/springmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.diogoscr.springmongo.repository;
+
+import com.diogoscr.springmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+
+
+
+}
