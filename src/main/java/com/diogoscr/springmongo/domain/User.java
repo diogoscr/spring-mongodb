efab660d088df5b26742e6eaf884688f87e092b4
diogoscr <diogo.arauj@gmail.com>
2021-03-07 19:50:02 -0300

    Using DTO pattern to return users

--- src/main/java/com/diogoscr/springmongo/domain/User.java
+++ src/main/java/com/diogoscr/springmongo/domain/User.java
@@ -2,7 +2,6 @@ package com.diogoscr.springmongo.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.io.Serializable;
 import java.util.Objects;
 
--- src/main/java/com/diogoscr/springmongo/resources/UserResource.java
+++ src/main/java/com/diogoscr/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.diogoscr.springmongo.resources;
 
 import com.diogoscr.springmongo.domain.User;
+import com.diogoscr.springmongo.dto.UserDTO;
 import com.diogoscr.springmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value ="/users")
@@ -17,9 +19,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
