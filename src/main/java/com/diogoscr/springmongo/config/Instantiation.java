f553768ed2edc4cabb06fbee42d0f7fe1b8841d4
diogoscr <diogo.arauj@gmail.com>
2021-03-19 19:33:21 -0300

    Adding comments to the posts

--- src/main/java/com/diogoscr/springmongo/config/Instantiation.java
+++ src/main/java/com/diogoscr/springmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.diogoscr.springmongo.config;
 import com.diogoscr.springmongo.domain.Post;
 import com.diogoscr.springmongo.domain.User;
 import com.diogoscr.springmongo.dto.AuthorDTO;
+import com.diogoscr.springmongo.dto.CommentDTO;
 import com.diogoscr.springmongo.repository.PostRepository;
 import com.diogoscr.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Nice trip, bro!", sdf.parse("12/02/2020"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Enjoy it!", sdf.parse("13/02/2020"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Have a great day!", sdf.parse("15/02/2020"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
